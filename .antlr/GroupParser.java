23766b480734046b9f5d70022a881b18ee2c171a
QueueTea <qthuet@gmail.com>
2022-12-02 20:24:26 -0600

    updated grammar

--- .antlr/GroupParser.java
+++ .antlr/GroupParser.java
@@ -16,26 +16,30 @@ public class GroupParser extends Parser {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		T__0=1, T__1=2, T__2=3, T__3=4, T__4=5, T__5=6, T__6=7, BYE=8, INT=9, 
-		WS=10;
+		T__0=1, T__1=2, Variable_expr=3, ID=4, INTEGER=5, ASSIGN=6, ADD=7, MINUS=8, 
+		MULTIPLY=9, DIVIDE=10, MOD=11, ADD_ASSIGN=12, SUB_ASSIGN=13, MUL_ASSIGN=14, 
+		DIV_ASSIGN=15, ASSIGNS=16, WHITESPACE=17;
 	public static final int
-		RULE_expr = 0;
+		RULE_single_input = 0, RULE_assignment_expr = 1, RULE_mathmatical_expr = 2;
 	private static String[] makeRuleNames() {
 		return new String[] {
-			"expr"
+			"single_input", "assignment_expr", "mathmatical_expr"
 		};
 	}
 	public static final String[] ruleNames = makeRuleNames();
 
 	private static String[] makeLiteralNames() {
 		return new String[] {
-			null, "'*'", "'/'", "'%'", "'+'", "'-'", "'('", "')'"
+			null, "'('", "')'", null, null, null, "'='", "'+'", "'-'", "'*'", "'/'", 
+			"'%'", "'+='", "'-='", "'*='", "'/='"
 		};
 	}
 	private static final String[] _LITERAL_NAMES = makeLiteralNames();
 	private static String[] makeSymbolicNames() {
 		return new String[] {
-			null, null, null, null, null, null, null, null, "BYE", "INT", "WS"
+			null, null, null, "Variable_expr", "ID", "INTEGER", "ASSIGN", "ADD", 
+			"MINUS", "MULTIPLY", "DIVIDE", "MOD", "ADD_ASSIGN", "SUB_ASSIGN", "MUL_ASSIGN", 
+			"DIV_ASSIGN", "ASSIGNS", "WHITESPACE"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -89,162 +93,233 @@ public class GroupParser extends Parser {
 		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
 	}
 
-	public static class ExprContext extends ParserRuleContext {
-		public ExprContext(ParserRuleContext parent, int invokingState) {
+	public static class Single_inputContext extends ParserRuleContext {
+		public Assignment_exprContext assignment_expr() {
+			return getRuleContext(Assignment_exprContext.class,0);
+		}
+		public Mathmatical_exprContext mathmatical_expr() {
+			return getRuleContext(Mathmatical_exprContext.class,0);
+		}
+		public Single_inputContext(ParserRuleContext parent, int invokingState) {
 			super(parent, invokingState);
 		}
-		@Override public int getRuleIndex() { return RULE_expr; }
-	 
-		public ExprContext() { }
-		public void copyFrom(ExprContext ctx) {
-			super.copyFrom(ctx);
+		@Override public int getRuleIndex() { return RULE_single_input; }
+	}
+
+	public final Single_inputContext single_input() throws RecognitionException {
+		Single_inputContext _localctx = new Single_inputContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_single_input);
+		try {
+			setState(8);
+			_errHandler.sync(this);
+			switch ( getInterpreter().adaptivePredict(_input,0,_ctx) ) {
+			case 1:
+				enterOuterAlt(_localctx, 1);
+				{
+				setState(6);
+				assignment_expr();
+				}
+				break;
+			case 2:
+				enterOuterAlt(_localctx, 2);
+				{
+				setState(7);
+				mathmatical_expr(0);
+				}
+				break;
+			}
 		}
-	}
-	public static class NumberExprContext extends ExprContext {
-		public Token atom;
-		public TerminalNode INT() { return getToken(GroupParser.INT, 0); }
-		public NumberExprContext(ExprContext ctx) { copyFrom(ctx); }
-	}
-	public static class ByeExprContext extends ExprContext {
-		public Token atom;
-		public TerminalNode BYE() { return getToken(GroupParser.BYE, 0); }
-		public ByeExprContext(ExprContext ctx) { copyFrom(ctx); }
-	}
-	public static class ParenExprContext extends ExprContext {
-		public ExprContext expr() {
-			return getRuleContext(ExprContext.class,0);
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
 		}
-		public ParenExprContext(ExprContext ctx) { copyFrom(ctx); }
+		finally {
+			exitRule();
+		}
+		return _localctx;
 	}
-	public static class InfixExprContext extends ExprContext {
-		public ExprContext left;
+
+	public static class Assignment_exprContext extends ParserRuleContext {
+		public Token left;
 		public Token op;
-		public ExprContext right;
-		public List<ExprContext> expr() {
-			return getRuleContexts(ExprContext.class);
+		public Mathmatical_exprContext right;
+		public TerminalNode Variable_expr() { return getToken(GroupParser.Variable_expr, 0); }
+		public TerminalNode ASSIGNS() { return getToken(GroupParser.ASSIGNS, 0); }
+		public Mathmatical_exprContext mathmatical_expr() {
+			return getRuleContext(Mathmatical_exprContext.class,0);
 		}
-		public ExprContext expr(int i) {
-			return getRuleContext(ExprContext.class,i);
+		public Assignment_exprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
 		}
-		public InfixExprContext(ExprContext ctx) { copyFrom(ctx); }
+		@Override public int getRuleIndex() { return RULE_assignment_expr; }
 	}
 
-	public final ExprContext expr() throws RecognitionException {
-		return expr(0);
+	public final Assignment_exprContext assignment_expr() throws RecognitionException {
+		Assignment_exprContext _localctx = new Assignment_exprContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_assignment_expr);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(10);
+			((Assignment_exprContext)_localctx).left = match(Variable_expr);
+			setState(13);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			if (_la==ASSIGNS) {
+				{
+				setState(11);
+				((Assignment_exprContext)_localctx).op = match(ASSIGNS);
+				setState(12);
+				((Assignment_exprContext)_localctx).right = mathmatical_expr(0);
+				}
+			}
+
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
 	}
 
-	private ExprContext expr(int _p) throws RecognitionException {
+	public static class Mathmatical_exprContext extends ParserRuleContext {
+		public Mathmatical_exprContext left;
+		public Token op;
+		public Mathmatical_exprContext right;
+		public List<Mathmatical_exprContext> mathmatical_expr() {
+			return getRuleContexts(Mathmatical_exprContext.class);
+		}
+		public Mathmatical_exprContext mathmatical_expr(int i) {
+			return getRuleContext(Mathmatical_exprContext.class,i);
+		}
+		public TerminalNode INTEGER() { return getToken(GroupParser.INTEGER, 0); }
+		public TerminalNode Variable_expr() { return getToken(GroupParser.Variable_expr, 0); }
+		public TerminalNode MULTIPLY() { return getToken(GroupParser.MULTIPLY, 0); }
+		public TerminalNode DIVIDE() { return getToken(GroupParser.DIVIDE, 0); }
+		public TerminalNode MOD() { return getToken(GroupParser.MOD, 0); }
+		public TerminalNode ADD() { return getToken(GroupParser.ADD, 0); }
+		public TerminalNode MINUS() { return getToken(GroupParser.MINUS, 0); }
+		public Mathmatical_exprContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_mathmatical_expr; }
+	}
+
+	public final Mathmatical_exprContext mathmatical_expr() throws RecognitionException {
+		return mathmatical_expr(0);
+	}
+
+	private Mathmatical_exprContext mathmatical_expr(int _p) throws RecognitionException {
 		ParserRuleContext _parentctx = _ctx;
 		int _parentState = getState();
-		ExprContext _localctx = new ExprContext(_ctx, _parentState);
-		ExprContext _prevctx = _localctx;
-		int _startState = 0;
-		enterRecursionRule(_localctx, 0, RULE_expr, _p);
+		Mathmatical_exprContext _localctx = new Mathmatical_exprContext(_ctx, _parentState);
+		Mathmatical_exprContext _prevctx = _localctx;
+		int _startState = 4;
+		enterRecursionRule(_localctx, 4, RULE_mathmatical_expr, _p);
 		int _la;
 		try {
 			int _alt;
 			enterOuterAlt(_localctx, 1);
 			{
-			setState(9);
+			setState(22);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
-			case INT:
+			case T__0:
 				{
-				_localctx = new NumberExprContext(_localctx);
-				_ctx = _localctx;
-				_prevctx = _localctx;
-
-				setState(3);
-				((NumberExprContext)_localctx).atom = match(INT);
+				setState(16);
+				match(T__0);
+				setState(17);
+				mathmatical_expr(0);
+				setState(18);
+				match(T__1);
 				}
 				break;
-			case T__5:
+			case INTEGER:
 				{
-				_localctx = new ParenExprContext(_localctx);
-				_ctx = _localctx;
-				_prevctx = _localctx;
-				setState(4);
-				match(T__5);
-				setState(5);
-				expr(0);
-				setState(6);
-				match(T__6);
+				setState(20);
+				match(INTEGER);
 				}
 				break;
-			case BYE:
+			case Variable_expr:
 				{
-				_localctx = new ByeExprContext(_localctx);
-				_ctx = _localctx;
-				_prevctx = _localctx;
-				setState(8);
-				((ByeExprContext)_localctx).atom = match(BYE);
+				setState(21);
+				match(Variable_expr);
 				}
 				break;
 			default:
 				throw new NoViableAltException(this);
 			}
 			_ctx.stop = _input.LT(-1);
-			setState(19);
+			setState(32);
 			_errHandler.sync(this);
-			_alt = getInterpreter().adaptivePredict(_input,2,_ctx);
+			_alt = getInterpreter().adaptivePredict(_input,4,_ctx);
 			while ( _alt!=2 && _alt!=org.antlr.v4.runtime.atn.ATN.INVALID_ALT_NUMBER ) {
 				if ( _alt==1 ) {
 					if ( _parseListeners!=null ) triggerExitRuleEvent();
 					_prevctx = _localctx;
 					{
-					setState(17);
+					setState(30);
 					_errHandler.sync(this);
-					switch ( getInterpreter().adaptivePredict(_input,1,_ctx) ) {
+					switch ( getInterpreter().adaptivePredict(_input,3,_ctx) ) {
 					case 1:
 						{
-						_localctx = new InfixExprContext(new ExprContext(_parentctx, _parentState));
-						((InfixExprContext)_localctx).left = _prevctx;
-						pushNewRecursionContext(_localctx, _startState, RULE_expr);
-						setState(11);
+						_localctx = new Mathmatical_exprContext(_parentctx, _parentState);
+						_localctx.left = _prevctx;
+						_localctx.left = _prevctx;
+						pushNewRecursionContext(_localctx, _startState, RULE_mathmatical_expr);
+						setState(24);
 						if (!(precpred(_ctx, 5))) throw new FailedPredicateException(this, "precpred(_ctx, 5)");
-						setState(12);
-						((InfixExprContext)_localctx).op = _input.LT(1);
+						setState(25);
+						((Mathmatical_exprContext)_localctx).op = _input.LT(1);
 						_la = _input.LA(1);
-						if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << T__1) | (1L << T__2))) != 0)) ) {
-							((InfixExprContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+						if ( !((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << MULTIPLY) | (1L << DIVIDE) | (1L << MOD))) != 0)) ) {
+							((Mathmatical_exprContext)_localctx).op = (Token)_errHandler.recoverInline(this);
 						}
 						else {
 							if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
 							_errHandler.reportMatch(this);
 							consume();
 						}
-						setState(13);
-						((InfixExprContext)_localctx).right = expr(6);
+						setState(26);
+						((Mathmatical_exprContext)_localctx).right = mathmatical_expr(6);
 						}
 						break;
 					case 2:
 						{
-						_localctx = new InfixExprContext(new ExprContext(_parentctx, _parentState));
-						((InfixExprContext)_localctx).left = _prevctx;
-						pushNewRecursionContext(_localctx, _startState, RULE_expr);
-						setState(14);
+						_localctx = new Mathmatical_exprContext(_parentctx, _parentState);
+						_localctx.left = _prevctx;
+						_localctx.left = _prevctx;
+						pushNewRecursionContext(_localctx, _startState, RULE_mathmatical_expr);
+						setState(27);
 						if (!(precpred(_ctx, 4))) throw new FailedPredicateException(this, "precpred(_ctx, 4)");
-						setState(15);
-						((InfixExprContext)_localctx).op = _input.LT(1);
+						setState(28);
+						((Mathmatical_exprContext)_localctx).op = _input.LT(1);
 						_la = _input.LA(1);
-						if ( !(_la==T__3 || _la==T__4) ) {
-							((InfixExprContext)_localctx).op = (Token)_errHandler.recoverInline(this);
+						if ( !(_la==ADD || _la==MINUS) ) {
+							((Mathmatical_exprContext)_localctx).op = (Token)_errHandler.recoverInline(this);
 						}
 						else {
 							if ( _input.LA(1)==Token.EOF ) matchedEOF = true;
 							_errHandler.reportMatch(this);
 							consume();
 						}
-						setState(16);
-						((InfixExprContext)_localctx).right = expr(5);
+						setState(29);
+						((Mathmatical_exprContext)_localctx).right = mathmatical_expr(5);
 						}
 						break;
 					}
 					} 
 				}
-				setState(21);
+				setState(34);
 				_errHandler.sync(this);
-				_alt = getInterpreter().adaptivePredict(_input,2,_ctx);
+				_alt = getInterpreter().adaptivePredict(_input,4,_ctx);
 			}
 			}
 		}
@@ -261,12 +336,12 @@ public class GroupParser extends Parser {
 
 	public boolean sempred(RuleContext _localctx, int ruleIndex, int predIndex) {
 		switch (ruleIndex) {
-		case 0:
-			return expr_sempred((ExprContext)_localctx, predIndex);
+		case 2:
+			return mathmatical_expr_sempred((Mathmatical_exprContext)_localctx, predIndex);
 		}
 		return true;
 	}
-	private boolean expr_sempred(ExprContext _localctx, int predIndex) {
+	private boolean mathmatical_expr_sempred(Mathmatical_exprContext _localctx, int predIndex) {
 		switch (predIndex) {
 		case 0:
 			return precpred(_ctx, 5);
@@ -277,14 +352,17 @@ public class GroupParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\f\31\4\2\t\2\3\2"+
-		"\3\2\3\2\3\2\3\2\3\2\3\2\5\2\f\n\2\3\2\3\2\3\2\3\2\3\2\3\2\7\2\24\n\2"+
-		"\f\2\16\2\27\13\2\3\2\2\3\2\3\2\2\4\3\2\3\5\3\2\6\7\2\33\2\13\3\2\2\2"+
-		"\4\5\b\2\1\2\5\f\7\13\2\2\6\7\7\b\2\2\7\b\5\2\2\2\b\t\7\t\2\2\t\f\3\2"+
-		"\2\2\n\f\7\n\2\2\13\4\3\2\2\2\13\6\3\2\2\2\13\n\3\2\2\2\f\25\3\2\2\2\r"+
-		"\16\f\7\2\2\16\17\t\2\2\2\17\24\5\2\2\b\20\21\f\6\2\2\21\22\t\3\2\2\22"+
-		"\24\5\2\2\7\23\r\3\2\2\2\23\20\3\2\2\2\24\27\3\2\2\2\25\23\3\2\2\2\25"+
-		"\26\3\2\2\2\26\3\3\2\2\2\27\25\3\2\2\2\5\13\23\25";
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\23&\4\2\t\2\4\3\t"+
+		"\3\4\4\t\4\3\2\3\2\5\2\13\n\2\3\3\3\3\3\3\5\3\20\n\3\3\4\3\4\3\4\3\4\3"+
+		"\4\3\4\3\4\5\4\31\n\4\3\4\3\4\3\4\3\4\3\4\3\4\7\4!\n\4\f\4\16\4$\13\4"+
+		"\3\4\2\3\6\5\2\4\6\2\4\3\2\13\r\3\2\t\n\2(\2\n\3\2\2\2\4\f\3\2\2\2\6\30"+
+		"\3\2\2\2\b\13\5\4\3\2\t\13\5\6\4\2\n\b\3\2\2\2\n\t\3\2\2\2\13\3\3\2\2"+
+		"\2\f\17\7\5\2\2\r\16\7\22\2\2\16\20\5\6\4\2\17\r\3\2\2\2\17\20\3\2\2\2"+
+		"\20\5\3\2\2\2\21\22\b\4\1\2\22\23\7\3\2\2\23\24\5\6\4\2\24\25\7\4\2\2"+
+		"\25\31\3\2\2\2\26\31\7\7\2\2\27\31\7\5\2\2\30\21\3\2\2\2\30\26\3\2\2\2"+
+		"\30\27\3\2\2\2\31\"\3\2\2\2\32\33\f\7\2\2\33\34\t\2\2\2\34!\5\6\4\b\35"+
+		"\36\f\6\2\2\36\37\t\3\2\2\37!\5\6\4\7 \32\3\2\2\2 \35\3\2\2\2!$\3\2\2"+
+		"\2\" \3\2\2\2\"#\3\2\2\2#\7\3\2\2\2$\"\3\2\2\2\7\n\17\30 \"";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
--- Group.g4
+++ Group.g4
@@ -1,11 +1,77 @@
 grammar Group;
-expr: left=expr op=('*'|'/'|'%') right=expr    # InfixExpr
-    | left=expr op=('+'|'-') right=expr        # InfixExpr
-    | atom=INT                                 # NumberExpr
-    | '(' expr ')'                             # ParenExpr 
-    | atom=BYE                                 # ByeExpr
+/*
+ * Parser Rules
+ */
+
+single_input: assignment_expr
+            | mathmatical_expr
+            ;
+
+//assign_expr: ID ASSIGN arithmetic_expr;
+//augment_assign_expr: ID (ADD_ASSIGN | SUB_ASSIGN | MUL_ASSIGN | DIV_ASSIGN) arithmetic_expr;
+
+assignment_expr: left=Variable_expr (op=ASSIGNS right=mathmatical_expr)?;
+
+mathmatical_expr: left=mathmatical_expr op=(MULTIPLY | DIVIDE | MOD) right=mathmatical_expr  
+          | left=mathmatical_expr op=(ADD | MINUS) right=mathmatical_expr   
+          | '('mathmatical_expr')'
+          | INTEGER 
+          | Variable_expr;
+
+Variable_expr: ID;
+
+/*
+ * Lexer Rules
+ * Character interpreter and tokenizer
+ */
+
+ID         : ID_START ID_CONTINUE* ;
+INTEGER    : NON_ZERO_DIGIT DIGIT* 
+           | '0'
+           ;
+
+ASSIGN     : '=';
+ADD        : '+';
+MINUS      : '-';
+MULTIPLY   : '*';
+DIVIDE     : '/';
+MOD        : '%';
+ADD_ASSIGN : '+=';
+SUB_ASSIGN : '-=';
+MUL_ASSIGN : '*=';
+DIV_ASSIGN : '/=';
+
+ASSIGNS    : ASSIGN | ADD_ASSIGN | SUB_ASSIGN | MUL_ASSIGN | DIV_ASSIGN ;
+
+WHITESPACE : SPACES -> skip ;
+
+/*
+ * Fragments
+ * Base Characters to be used in lexer rules
+ */
+
+fragment NON_ZERO_DIGIT
+    : [1-9]
     ;
 
-BYE  : ('bye'| 'exit') ;
-INT  : [0-9]+;
-WS   : [ \t\r]+ -> skip ;
+fragment DIGIT
+    : [0-9]
+    ;
+
+fragment SPACES
+    : [ \t\r]+
+    ;
+
+fragment ID_START
+    : [A-Z]
+    | [a-z]
+    | '_'
+    ;
+
+fragment ID_CONTINUE
+    : ID_START
+    | [A-Z]
+    | [a-z]
+    | '_'
+    | [0-9]
+    ;
